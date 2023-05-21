183bce31eff3e1276e5852b140c4b9fdc84f3c74
AQUIB ABDULJABBAR MULANI <43015898+aquibmulani@users.noreply.github.com>
2023-05-21 07:00:32 +0000

    added solution to horesplay

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -16,7 +16,44 @@
 //           knight: The knight's location.
 // Returns: An STL vector of strings with the possible locations to move.
 std::vector<std::string> knight_moves(std::string knight){
-    std::vector<std::string> moves;
+    std::vector<std::string> moves;    
+    int x, y;
+    x = knight[0] - 'a'+ 1;
+    y = knight[1] - '0';
+    //std::cout<<(knight[0] - 'a'+1)<<"\n";
+
+    int delta1[2] = {-1,1};
+    int delta2[2] = {-2,2};
+    std::string new_loc = "  ";
+    int new_x;
+    int new_y;
+    for(int i =0; i < 2;i++)
+    {
+        for(int j=0; j < 2;j++)
+        {
+            new_x = x + delta1[i];
+            new_y = y + delta1[j];
+
+            if(new_x > 0 && new_x  < 9 && new_y > 0 && new_y < 9)
+            {
+                new_loc[0] = new_x + 'a'-1;
+                new_loc[1] = new_y + '0';
+                moves.push_back(new_loc);
+
+            }
+            new_x = x + delta2[i];
+            new_y = y + delta1[j];
+            if(new_x > 0 && new_x  < 9 && new_y > 0 && new_y < 9)
+            {
+                new_loc[0] = new_x + 'a'-1;
+                new_loc[1] = new_y + '0';
+                moves.push_back(new_loc);
+
+            }
+        }
+        return moves;
+    }
+
 
     // Write your code here
 
